32483a7716c6e411d5221c58eabbbd46b9537a7d
Tahmin Fatiyev <tahmin.fatiyev@gmail.com>
2022-02-05 15:07:50 +0400

    Final Final Stage

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,23 +2,26 @@ let leftIcon = document.querySelector(".left-icon");
 let rightIcon = document.querySelector(".right-icon");
 let sliderList = document.querySelector(".slider-scroll");
 
-rightIcon.addEventListener("click", function(){
-    let matrix = window.getComputedStyle(sliderList).getPropertyValue("transform");
-    let matrixArr = matrix.split(", ");
-    let translateXNum = parseInt(matrixArr[4]);
-    if (translateXNum > -1344) {
-        sliderList.style.transform = "translateX(" + (translateXNum - 336) + "px)";
+
+rightIcon.addEventListener("click", function rightIcon(){
+    let valueProp = window.getComputedStyle(sliderList).getPropertyValue("transform");
+    let valueArr = valueProp.split(", ");
+    let translateXValue = parseInt(valueArr[4]);
+    if (translateXValue > -1344) {
+        sliderList.style.transform = "translateX(" + (translateXValue - 336) + "px)";
+        return;
     }
     else{
         return;
     }
 })
-leftIcon.addEventListener("click", function(){
-    let matrix = window.getComputedStyle(sliderList).getPropertyValue("transform");
-    let matrixArr = matrix.split(", ");
-    let translateXNum = parseInt(matrixArr[4]);
-    if (translateXNum < 0) {
-        sliderList.style.transform = "translateX(" + (translateXNum + 336) + "px)";
+leftIcon.addEventListener("click", function leftIcon(){
+    let valueProp = window.getComputedStyle(sliderList).getPropertyValue("transform");
+    let valueArr = valueProp.split(", ");
+    let translateXValue = parseInt(valueArr[4]);
+    if (translateXValue < 0) {
+        sliderList.style.transform = "translateX(" + (translateXValue + 336) + "px)";
+        return;
     }
     else{
         return;
